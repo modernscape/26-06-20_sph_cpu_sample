15c87d2f56f8d38773a95638e71a0a490ccb69c7
modernscape <modernscape@gmail.com>
2026-07-08 21:52:37 +0900

    26-07-08_

--- main.js
+++ main.js
@@ -21,3 +21,47 @@ document.getElementById("app").appendChild(renderer.domElement)
 // const material = new THREE.PointsMaterial({ size: 0.2, vertexColors: true })
 // const points = new THREE.Points(geometry, material)
 // scene.add(points)
+
+/******************************************************
+ * 定数
+ *****************************************************/
+const MAX_PARTICLES = 5000
+
+/******************************************************
+ * データ構造
+ *****************************************************/
+// Position
+// Velocity
+// Force
+// Density
+// Pressure
+// Mass
+// Color
+// Lambda
+// DeltaPosition
+
+const posX = new Float32Array(MAX_PARTICLES)
+const posY = new Float32Array(MAX_PARTICLES)
+const posZ = new Float32Array(MAX_PARTICLES)
+
+const velX = new Float32Array(MAX_PARTICLES)
+const velY = new Float32Array(MAX_PARTICLES)
+const velZ = new Float32Array(MAX_PARTICLES)
+
+const forceX = new Float32Array(MAX_PARTICLES)
+const forceY = new Float32Array(MAX_PARTICLES)
+const forceZ = new Float32Array(MAX_PARTICLES)
+
+const density = new Float32Array(MAX_PARTICLES)
+const pressure = new Float32Array(MAX_PARTICLES)
+const mass = new Float32Array(MAX_PARTICLES)
+
+const colorR = new Float32Array(MAX_PARTICLES)
+const colorG = new Float32Array(MAX_PARTICLES)
+const colorB = new Float32Array(MAX_PARTICLES)
+
+// Position Based Fluids
+const lambda = new Float32Array(MAX_PARTICLES)
+const deltaPosX = new Float32Array(MAX_PARTICLES)
+const deltaPosY = new Float32Array(MAX_PARTICLES)
+const deltaPosZ = new Float32Array(MAX_PARTICLES)
